9b0166d06c8a4d80bd5a5e18cd8b2039bc1e652d
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-09-11 15:05:05 -0300

    validations completed

---
+++ server/src/models/schemas/post.schema.js
@@ -0,0 +1,22 @@
+import { body } from "express-validator";
+
+export const createPost = [
+  body("title")
+    .notEmpty()
+    .isString()
+    .withMessage("Title is required and must be a string."),
+  body("content")
+    .notEmpty()
+    .isString()
+    .withMessage("Content is required and must be a string."),
+  body("imgUrl").isURL().withMessage("The image URL is not valid."),
+];
+
+export const updatePost = [
+  body("title").optional().isString().withMessage("Title must be a string."),
+  body("content")
+    .optional()
+    .isString()
+    .withMessage("Content must be a string."),
+  body("imgUrl").optional().isURL().withMessage("The image URL is not valid."),
+];
