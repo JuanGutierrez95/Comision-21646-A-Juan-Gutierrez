4d96c74b25b2d00d701b2517780e2e3f8149de23
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-09-11 10:22:23 -0300

    new changes in drivers folder

--- server/src/controllers/post.controllers.js
+++ server/src/controllers/post.controllers.js
@@ -1,4 +1,15 @@
-export const getPosts = (req, res) => {};
-export const createPost = (req, res) => {};
-export const updatePost = (req, res) => {};
-export const deletePost = (req, res) => {};
+export const getPosts = (req, res) => {
+    res.status(200).json('hola por todos los posts')
+};
+export const createPost = (req, res) => {
+
+};
+export const updatePost = (req, res) => {
+
+};
+export const deletePost = (req, res) => {
+
+};
+
+
+//SEGUIMOS MAS TARDE POR ACA
