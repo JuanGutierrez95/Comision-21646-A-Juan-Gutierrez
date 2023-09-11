9d040b19a0f03dc2675bd1433ee5ac49f4d17017
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-09-11 15:06:00 -0300

    middlewares completed

---
+++ server/src/middlewares/validator.js
@@ -0,0 +1,8 @@
+import { validationResult } from "express-validator";
+
+export const validator = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+};
