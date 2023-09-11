7606d052c7826b733c46db2b39ce4d1ed06db2dc
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-09-11 00:15:41 -0300

    startDb was imported

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { postRouter } from "./src/routes/post.routes.js";
+import { startDb } from "./src/config/database.js";
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use("/", postRouter)
 
 app.listen(PORT, () => {
     console.log(`server listening http://localhost:${PORT}`)
+    startDb()
 })
 
