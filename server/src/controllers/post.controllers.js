7e138ca2cebae5f898511a872664a3e30caf7b89
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-09-11 11:05:29 -0300

    Controller for all posts

--- server/src/controllers/post.controllers.js
+++ server/src/controllers/post.controllers.js
@@ -1,15 +1,46 @@
-export const getPosts = (req, res) => {
-    res.status(200).json('hola por todos los posts')
-};
-export const createPost = (req, res) => {
+import { PostModel } from "../models/Posts.js";
 
+
+export const getPosts = async (req, res) => {
+    try {
+        const posts = await PostModel.findAll();
+        if(!posts){
+            return res.status(404)
+        }else {
+            return res.status(200).json(posts)
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: error.message });
+    }
+};
+export const createPost = async (req, res) => {
+
+    try {
+        const { title, content, imgUrl } = req.body
+        const post = await PostModel.create({
+            
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: error.message });
+    }
 };
 export const updatePost = (req, res) => {
-
+    try {
+        
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: error.message });
+    }
 };
 export const deletePost = (req, res) => {
-
-};
-
+    try {
+        
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: error.message });
+    }
+}
 
 //SEGUIMOS MAS TARDE POR ACA
