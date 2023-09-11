3b65d9689df7490a3b9932a69230f37b4ff7dc85
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-09-11 00:13:10 -0300

    Updated database configuration file

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -4,3 +4,14 @@ export const sequelize = new Sequelize("db_posts", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
+
+export const startDb = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        //await sequelize.sync({force: true});
+        console.log("Connection established successfully.")
+    } catch (error) {
+        console.log("Unable to connect to the database:", error)
+    }
+}
