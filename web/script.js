d8fee59ceab2bfb18d2326f175a1d3dfd4015e1a
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-09-13 15:19:41 -0300

    se agregó contenedor para que se muestre en la web

--- web/script.js
+++ web/script.js
@@ -1,7 +1,35 @@
-//Solicitudes de Red 
+//Solicitudes de Red
+
+const contenedor = document.getElementById("container-row");
+console.log(contenedor);
+
+let html = "";
+
 fetch("http://localhost:5000/api/posts")
-    .then((response) => response.json()) // Parse la respuesta como JSON
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => console.error("Error:", error));
+  .then((response) => response.json()) // Parse la respuesta como JSON
+  .then((data) => {
+    console.log(data);
+    data.forEach((post) => {
+      html += `
+        <article class="col-4 d-flex justify-content-center mb-3" data-id="${post.id}">
+            <!--Post-->
+            <div class="card" style="width: 18rem">
+              <img
+                src="${post.imgUrl}"
+                class="card-img-top"
+                alt="nuevo titulo"
+              />
+              <div class="card-body">
+                <h5 class="card-title">${post.title}</h5>
+                <p class="card-text">${post.content}</p>
+                <div>
+                  <button class="btn btn-secondary">Edit</button>
+                  <button class="btn btn-danger">Delete</button>
+                </div>
+              </div>
+            </div>
+          </article>
+        `;
+      contenedor.innerHTML = html;
+    });
+  });
