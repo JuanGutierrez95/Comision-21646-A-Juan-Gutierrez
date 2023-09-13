289e3d55b6ef4bc71f3949dea7f853c45f1042b5
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-09-13 11:13:09 -0300

    se agregó cors

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { postRouter } from "./src/routes/post.routes.js";
 import { startDb } from "./src/config/database.js";
+import cors from "cors";
 
 const app = express();
 
+//middlewares
 app.use(express.json());
+app.use(cors());
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.use("/", postRouter);
 
