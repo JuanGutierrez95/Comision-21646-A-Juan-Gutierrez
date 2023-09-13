64651976bf94d6a3085780fd2a8e37822a570a1e
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-09-13 20:18:46 -0300

    Agregada funcionalidad de eliminar y editar

--- web/script.js
+++ web/script.js
@@ -15,10 +15,78 @@ const inputImg = document.getElementById("inputImg");
 btnCrear.addEventListener("click", () => {
   option = "new";
   btnSave.textContent = "New";
-  inputTitle.value = ""
-  inputContent.value = ""
-  inputImg.value = ""
+  inputTitle.value = "";
+  inputContent.value = "";
+  inputImg.value = "";
   myModal.show();
+});
+
+document.addEventListener("click", (event) => {
+  if (event.target.matches("#btn-delete")) {
+    const article = event.target.closest(".col-4");
+    const idArticle = article.dataset.id;
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/api/posts/${idArticle}`, {
+          method: "DELETE",
+        })
+          .then((res) => {
+            if (res.ok) {
+              article.remove();
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
+      }
+    });
+  }
+});
+
+/*
+document.addEventListener('click', (event) => {
+  if(event.target.matches('#btn-delete')){
+    const article = event.target.closest('.col-4')
+    const idArticle = article.dataset.id
+    
+    fetch(`http://localhost:5000/api/posts/${idArticle}`,{
+      method: 'DELETE'
+    }).then(res => {
+      if(res.ok){
+        article.remove()
+      }
+    }).catch(err => {
+      console.error(err)
+    })
+  }
+})
+*/
+
+document.addEventListener("click", (event) => {
+  if (event.target.matches("#btn-edit")) {
+    const article = event.target.closest(".col-4");
+    const idArticle = article.dataset.id;
+    const urlPostEdit = article.children[0].children[0].src;
+    const titleEdit = article.children[0].children[1].children[0].textContent;
+    const contentEdit = article.children[0].children[1].children[1].textContent;
+
+    inputTitle.value = titleEdit;
+    inputContent.value = contentEdit;
+    inputImg.value = urlPostEdit;
+    option = "edit";
+    btnSave.textContent = "edit";
+    myModal.show();
+  }
 });
 
 fetch("http://localhost:5000/api/posts")
@@ -39,8 +107,8 @@ fetch("http://localhost:5000/api/posts")
                 <h5 class="card-title">${post.title}</h5>
                 <p class="card-text">${post.content}</p>
                 <div>
-                  <button class="btn btn-secondary">Edit</button>
-                  <button type="" class="btn btn-danger">Delete</button>
+                  <button class="btn btn-secondary" id="btn-edit">Edit</button>
+                  <button type="" class="btn btn-danger" id="btn-delete">Delete</button>
                 </div>
               </div>
             </div>
