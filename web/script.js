8360e85ca34edaf8d61c9e19ccfa3429f2a98cd2
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-09-13 16:04:27 -0300

    se trabajó en la parte de los inputs

--- web/script.js
+++ web/script.js
@@ -2,8 +2,24 @@
 
 const contenedor = document.getElementById("container-row");
 console.log(contenedor);
-
+const btnCrear = document.getElementById("btn-new");
+const myModal = new bootstrap.Modal(document.getElementById("myModal"));
+const btnSave = document.getElementById("btn-save");
 let html = "";
+let option = "";
+
+const inputTitle = document.getElementById("inputTitle");
+const inputContent = document.getElementById("inputContent");
+const inputImg = document.getElementById("inputImg");
+
+btnCrear.addEventListener("click", () => {
+  option = "new";
+  btnSave.textContent = "New";
+  inputTitle.value = ""
+  inputContent.value = ""
+  inputImg.value = ""
+  myModal.show();
+});
 
 fetch("http://localhost:5000/api/posts")
   .then((response) => response.json()) // Parse la respuesta como JSON
@@ -24,7 +40,7 @@ fetch("http://localhost:5000/api/posts")
                 <p class="card-text">${post.content}</p>
                 <div>
                   <button class="btn btn-secondary">Edit</button>
-                  <button class="btn btn-danger">Delete</button>
+                  <button type="" class="btn btn-danger">Delete</button>
                 </div>
               </div>
             </div>
