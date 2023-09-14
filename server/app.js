1be94b54fa009d7c697e1ecef3ad5219d78852e8
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-09-14 13:50:14 -0300

    se configuró para el uso de ejs

--- server/app.js
+++ server/app.js
@@ -1,13 +1,23 @@
 import express from "express";
 import { postRouter } from "./src/routes/post.routes.js";
 import { startDb } from "./src/config/database.js";
+import path from "node:path";
 import cors from "cors";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 //middlewares
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "src", "public")));
+
+app.set("views", path.join(__dirname, "src", "views"));
+app.set("view engine", "ejs");
 
 const PORT = process.env.PORT || 3000;
 
