e10c72f4188827c3ceee6e6fccea203f5c5e84a0
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-09-14 14:21:14 -0300

    se importó el controller de vista y se configuró la ruta de la misma

--- server/src/routes/post.routes.js
+++ server/src/routes/post.routes.js
@@ -4,11 +4,14 @@ import {
   ctrlDeletePost,
   ctrlGetPosts,
   ctrlUpdatePost,
+  ctrlView,
 } from "../controllers/post.controllers.js";
 import { createPost, editPost } from "../models/schemas/post.schema.js";
 import { validator } from "../middlewares/validator.js";
 
 const postRouter = Router();
+
+postRouter.get("/posts", ctrlView);
 
 postRouter.get("/api/posts", ctrlGetPosts);
 postRouter.post("/api/posts", createPost, validator, ctrlCreatePost);
